e4ab413d0f9cc6a6ed6dcb8ced4c64b96ed54826
codehass <hassan.elouardy06@gmail.com>
2024-05-15 19:36:00 +0100

    create button Icon component

---
+++ src/ui/ButtonIcon.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ButtonIcon = styled.button`
+	background: none;
+	border: none;
+	transition: all 0.2s;
+
+	& svg {
+		color: var(--color-brand-100);
+	}
+`;
+
+export default ButtonIcon;
