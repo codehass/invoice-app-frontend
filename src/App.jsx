41640680b68fccdb5640ce15f045ec32f882457b
codehass <hassan.elouardy06@gmail.com>
2024-05-15 18:22:25 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
-	return <div>Invoice app</div>;
+	return (
+		<>
+			<GlobalStyles />
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AppLayout />}>
+						<Route path="/" element={<h1>This is the main routes</h1>} />
+						<Route
+							path="/invoices"
+							element={<h1>This is the main routes</h1>}
+						/>
+						<Route
+							path="/invoices/:invoiceId"
+							element={<h1>This is details for single invoice</h1>}
+						/>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
