da59b75e467efa800c9db7aecfe1731551f0d923
codehass <hassan.elouardy06@gmail.com>
2024-05-15 18:22:59 +0100

    create sidebar

---
+++ src/ui/SideBar.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+	background-color: var(--color-gray-100);
+	color: red;
+`;
+
+function SideBar() {
+	return <StyledContainer>SideBar</StyledContainer>;
+}
+
+export default SideBar;
