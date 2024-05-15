84c88b81e3028030da10f280560f16a65e66c6e2
codehass <hassan.elouardy06@gmail.com>
2024-05-15 19:37:15 +0100

    finish side bar component

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,12 +1,36 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import DarkModeToggle from "./DarkModeToggle";
+import UserLogo from "./UserLogo";
 
 const StyledContainer = styled.div`
 	background-color: var(--color-gray-100);
-	color: red;
+	border-top-right-radius: 20px;
+	border-bottom-right-radius: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 1rem;
+	/* padding: 0 0.5rem 1rem 0.5rem; */
+	padding-bottom: 1.5rem;
 `;
 
 function SideBar() {
-	return <StyledContainer>SideBar</StyledContainer>;
+	return (
+		<StyledContainer>
+			<Logo />
+			<Container>
+				<DarkModeToggle />
+				<UserLogo />
+			</Container>
+		</StyledContainer>
+	);
 }
 
 export default SideBar;
