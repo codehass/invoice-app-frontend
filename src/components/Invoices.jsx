416a0f70600c2322d63f98836efd2c5a047c6a79
codehass <hassan.elouardy06@gmail.com>
2024-05-16 19:34:09 +0100

    create invoices component

---
+++ src/components/Invoices.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const InvoicesContainer = styled.div`
+	padding-top: 2rem;
+`;
+function Invoices() {
+	return <InvoicesContainer>Invoices list</InvoicesContainer>;
+}
+
+export default Invoices;
