342767d72da48d3d5b64409773d52dfb025da43f
codehass <hassan.elouardy06@gmail.com>
2024-05-18 22:11:33 +0100

    separate components

--- src/components/InvoiceItem.jsx
+++ src/components/InvoiceItem.jsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import { MdNavigateNext } from "react-icons/md";
 
+import InvoicePrice from "../ui/InvoicePrice";
+import InvoiceStatus from "../ui/InvoiceStatus";
+import InvoiceInfo from "../ui/InvoiceInfo";
+
 const StyledInvoice = styled.div`
 	display: flex;
 	justify-content: space-between;
@@ -8,60 +12,19 @@ const StyledInvoice = styled.div`
 	background: #1f203c;
 	padding: 30px 20px;
 	border-radius: 15px;
-	border: 4px solid transparent;
+	border: 2px solid transparent;
 
 	&:hover {
 		cursor: pointer;
-		border-color: #070946;
+		border-color: #7d5cf9;
 	}
 `;
 
-const InfoInvoice = styled.div`
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
-	/* gap: 25px; */
-	width: 55%;
-
-	& h5 {
-		font-size: 1.4rem;
-		color: #f6f6ff;
-	}
-
-	& span {
-		font-size: 1.3rem;
-		font-weight: 100;
-		color: #8789a3;
-	}
-`;
-
-const StatusAndPaid = styled.div`
+const InvoiceActions = styled.div`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 	width: 40%;
-`;
-
-const Status = styled.div`
-	padding: 5px 20px;
-	border-radius: 5px;
-	background: #1f2c3f;
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
-	width: 120px;
-	/* gap: 15px; */
-
-	& span {
-		background: #38d4a5;
-		width: 10px;
-		height: 10px;
-		border-radius: 50%;
-	}
-
-	& p {
-		color: #38d4a5;
-	}
 `;
 
 function InvoiceItem({ itemInfo }) {
@@ -69,20 +32,13 @@ function InvoiceItem({ itemInfo }) {
 
 	return (
 		<StyledInvoice>
-			<InfoInvoice>
-				<h5>{id}</h5>
-				<span>{date}</span>
-				<span>{name}</span>
-			</InfoInvoice>
+			<InvoiceInfo id={id} date={date} name={name} />
 
-			<StatusAndPaid>
-				<h3>£{price}</h3>
-				<Status>
-					<span></span>
-					<p>{status}</p>
-				</Status>
+			<InvoiceActions>
+				<InvoicePrice price={price} />
+				<InvoiceStatus status={status} />
 				<MdNavigateNext style={{ color: "#7D5CF9" }} />
-			</StatusAndPaid>
+			</InvoiceActions>
 		</StyledInvoice>
 	);
 }
