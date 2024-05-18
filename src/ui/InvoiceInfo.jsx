291fd10873d5c589f392f1208dcd29ccd4817205
codehass <hassan.elouardy06@gmail.com>
2024-05-18 22:11:49 +0100

    create InvoiceInfo component

---
+++ src/ui/InvoiceInfo.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const InfoInvoiceWrapper = styled.div`
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	width: 55%;
+
+	& h5 {
+		font-size: 1.4rem;
+		color: #f6f6ff;
+	}
+
+	& span {
+		font-size: 1.3rem;
+		font-weight: 100;
+		color: #8789a3;
+	}
+`;
+
+const InvoiceInfo = ({ id, date, name }) => (
+	<InfoInvoiceWrapper>
+		<h5>#{id}</h5>
+		<span>{date}</span>
+		<span>{name}</span>
+	</InfoInvoiceWrapper>
+);
+
+export default InvoiceInfo;
